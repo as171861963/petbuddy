dab29dd05780f9b7d729644764e230731dc0bf52
taolinjun <taolinjun.good@163.com>
2019-07-18 15:23:04 +0800

    tt

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
+import Foot  from  "./component/foot/addFoot.vue"
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
@@ -11,5 +12,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+   render: h => h(App)
 }).$mount('#app')
