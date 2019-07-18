9a0bd63f7b27aedae6e1468a29449aa614577c9d
MrStuarty <137421236@qq.com>
2019-07-18 14:42:19 +0800

    last

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import _g from "./_globals"
-import App from './components/pets.vue'
+import App from './App'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
