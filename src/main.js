d87e601f8cf5d360afac52ce76931bafdbc8b975
chentao <171861963@qq.com>
2019-07-18 10:04:20 +0800

    陈涛修改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './views/info'
 import router from './router'
 import store from './store'
+import _g from "./_globals"
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
--- src/router.js
+++ src/router.js
@@ -18,6 +18,19 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/info',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/info.vue'),
+      children:[{
+        path: 'commodity',
+        name: 'commodity',
+        component: () => import(/* webpackChunkName: "about" */ './components/shop/commodity.vue'),
+      }]
     }
   ]
 })
