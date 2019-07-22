6fd516c99ab3aa5a115fdea063b09c536b0da5cf
chentao <171861963@qq.com>
2019-07-22 17:55:17 +0800

    231

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,15 +1,40 @@
 import userApi from "../services/usersServices.js";
 
 export default {
-    namespaced:true,
-    actions:{
-        async signInAsync(context,payload){
+    namespaced: true,
+    state: {
+        rows: []
+    },
+    mutations: {
+        combineRows(state, payload) {
+            state.rows = payload;
+        },
+        deleteItem(state, payload) {
+            state.rows.splice(payload, 1);
+        }
+    },
+    actions: {
+        async signInAsync(context, payload) {
             const { data } = await userApi.signIn(payload);
             return data;
         },
-        async loginAsync(context,payload){
+        async loginAsync(context, payload) {
             const { data } = await userApi.login(payload);
             return data;
+        },
+        async getUserManageAsync({ commit }, payload) {
+            const { data } = await userApi.getUserManage(payload);
+            commit("combineRows", data)
+        },
+
+        async changeUserStatusAsync({ commit }, payload) {
+            const { data } = await userApi.changeUserStatus({
+                _id: payload._id,
+                newAttr: payload.newAttr
+            });
+            if (data.ok > 0) {
+                commit("deleteItem", payload.index)
+            }
         }
     }
 }
